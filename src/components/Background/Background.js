3033b22efa6088e4ae018b1013252c6a11a25faf
Anthony <anthony.cattet@gmail.com>
2022-08-03 22:18:53 -0400

    background stuff

---
+++ src/components/Background/Background.js
@@ -0,0 +1,10 @@
+import React from "react";
+import background from "./image/gengar.png";
+
+function App() {
+    return (
+      <div style={{ backgroundImage: `url(${background})` }}>
+        Hello World
+      </div>
+    );
+  }
